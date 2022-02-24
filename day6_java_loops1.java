90f9039862db42952a1314eddf0f94f5fcbc70b6
ASRULD0221533 <100027706+ASRULD0221533@users.noreply.github.com>
2022-02-24 15:59:01 +0700

    Add files via upload

---
+++ day6_java_loops1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Rullcoding;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class day6_java_loops1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        for(int i=1;i<=10;i++){
+            int result = N * i;
+            System.out.println(N+" x "+i+" = "+result);
+        }
+
+        bufferedReader.close();
+    }
+}
